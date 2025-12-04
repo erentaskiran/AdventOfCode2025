8ebccce8edac36548e8b64962b69695cce8b49e8
Eren Taskiran <eren.tas3535@gmail.com>
2025-12-04 09:54:43 +0300

    day4

---
+++ day4/ans1.java
@@ -0,0 +1,43 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class ans1 {
+
+    public static void main(String[] args) {
+        int ans=0;
+        String[] lines = null;
+        try{
+            List<String> tmp = Files.readAllLines(Paths.get("inp1.txt"));
+            lines = tmp.toArray(new String[0]);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        for(int i=0; i<lines.length; i++){
+            for(int j=0; j<lines[0].length(); j++){
+                if(lines[i].charAt(j) != '@'){
+                    continue;
+                }
+
+                int count = 0;
+                for(int k = i-1; k<=i+1; k++){
+                    for(int l = j-1; l<=j+1; l++){
+                        if(k == i && l == j){
+                            continue;
+                        }
+                        if(k>=0 && k<lines.length && l>=0 && l<lines[0].length()){
+                            if(lines[k].charAt(l) == '@' || lines[k].charAt(l) == 'x'){
+                                count++;
+                            }
+                        }
+                    }
+                }
+                if(count < 4){
+                    ans++;
+                    lines[i] = lines[i].substring(0, j) + 'x' + lines[i].substring(j + 1);
+                }
+            }
+        }
+        System.out.println(ans);
+    }
+}
